88fd87f7ea79f18b8dc5623762e4c4f4b4e33eef
Hyun Joon Kim <hjkim74@myseneca.ca>
2025-08-13 12:33:43 -0400

    hello

--- api/server.js
+++ api/server.js
@@ -1,23 +1,11 @@
-// server.js
+// api/index.js
 const express = require("express");
-const app = express();
-
 const serverless = require("serverless-http");
 
-app.use(express.json());
+const app = express();
 
 app.get("/", (req, res) => {
   res.json({ message: "API Listening" });
 });
 
-// app.get("/api/health", (req, res) => {
-//   res.json({ status: "Server is running!" });
-// });
-
-// app.post("/api/echo", (req, res) => {
-//   res.json({ received: req.body });
-// });
-
-// Export as Vercel serverless function
 module.exports = serverless(app);
-
